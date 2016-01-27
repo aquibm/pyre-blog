dbca0650f506b0b04de47687aa1a0d3466323a2e
Aquib Master <aquibmaster@gmail.com>
2016-01-27 21:21:11 +1300

    Tidy up layout and make navbar functional.

--- src/app/components/navbar/navigation.controller.ts
+++ src/app/components/navbar/navigation.controller.ts
@@ -1,9 +1,35 @@
-export class NavigationController {
+interface INavigationLink {
+	name: string;
+	ref: string;
+	icon?: string;
+	external?: boolean;
+	target?: string;
+}
 
-	private links = ['Home', 'About', 'Contact'];
+export class NavigationController {
+	public links: Array<INavigationLink>;
 
 	/* @ngInject */
-	constructor(private $mdSidenav: any) { }
+	constructor(private $mdSidenav: any) {
+		this.links = [
+			{
+				name: 'Home',
+				ref: 'home'
+			},
+
+			{
+				name: 'About',
+				ref: 'about'
+			},
+
+			{
+				name: 'Github',
+				ref: 'https://github.com/aquibm/pyre-blog',
+				external: true,
+				target: '_blank'
+			}
+		];
+	}
 
 	public toggleSideNav(): void {
 		this.$mdSidenav('left').toggle();
--- src/app/index.route.ts
+++ src/app/index.route.ts
@@ -1,12 +1,12 @@
 /** @ngInject */
 export function routerConfig($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/home/home.html',
-      controller: 'HomeController',
-      controllerAs: 'vm'
-    });
+	$stateProvider
+	.state('home', {
+		url: '/',
+		templateUrl: 'app/home/home.html',
+		controller: 'HomeController',
+		controllerAs: 'vm'
+	});
 
-  $urlRouterProvider.otherwise('/');
+	$urlRouterProvider.otherwise('/');
 }

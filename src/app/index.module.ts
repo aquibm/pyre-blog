0dd4fa4473a8b21cac44de139fd2b16a9147cab8
Aquib Master <aquibmaster@gmail.com>
2016-01-30 11:35:02 +1300

    Finish basic site layout and blog page.

--- src/app/index.module.ts
+++ src/app/index.module.ts
@@ -1,15 +1,9 @@
 /// <reference path="../../.tmp/typings/tsd.d.ts" />
 
-// Config
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
-
-// Services
-
-// Controllers
-import { HomeController } from './home/home.controller';
-import { NavigationController } from './components/navbar/navigation.controller';
+import { MainController } from './main/main.controller';
 
 declare var moment: moment.MomentStatic;
 
@@ -17,18 +11,9 @@ module pyreBlog {
 	'use strict';
 
 	angular.module('pyreBlog', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ngMaterial', 'toastr'])
-
-	// Constants
-	.constant('moment', moment)
-
-	// Config
-	.config(config)
-	.config(routerConfig)
-	.run(runBlock)
-
-	// Services
-
-	// Controllers
-	.controller('HomeController', HomeController)
-	.controller('NavigationController', NavigationController);
+		.constant('moment', moment)
+		.config(config)
+		.config(routerConfig)
+		.run(runBlock)
+		.controller('MainController', MainController);
 }
--- src/app/index.route.ts
+++ src/app/index.route.ts
@@ -1,12 +1,12 @@
 /** @ngInject */
 export function routerConfig($stateProvider: angular.ui.IStateProvider, $urlRouterProvider: angular.ui.IUrlRouterProvider) {
-	$stateProvider
-	.state('home', {
-		url: '/',
-		templateUrl: 'app/home/home.html',
-		controller: 'HomeController',
-		controllerAs: 'vm'
-	});
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/main/main.html',
+      controller: 'MainController',
+      controllerAs: 'main'
+    });
 
-	$urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/');
 }
